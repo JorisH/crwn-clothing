20c14698ce22b14544f2fffb13b68bacb0acc09c
Joris Hontelé <joris@tactics.be>
2021-12-17 21:45:59 +0100

    Added signin form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './page/homepage/homepage.component';
 import './App.css';
-import ShopPage from './page/shoppage/shop.component';
+import ShopPage from './page/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './page/sing-in-and-sign-up/sing-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,8 @@ const Header = () => (
     <Link className='logo-container' to="/"><Logo className='logo' /></Link>
     <div className='options'>
       <Link className='option' to="/shop">SHOP</Link>
-      <Link className='option' to="/shop">CONTACT</Link>
+      <Link className='option' to="/contact">CONTACT</Link>
+      <Link className='option' to="/signin">SIGN IN</Link>
     </div>
   </div>
 )

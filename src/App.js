416bfd58027e51e3926a527e297e37c2945a6cdd
Joris Hontelé <joris@tactics.be>
2021-12-18 14:28:02 +0100

    finishing google sign-in/out and using react hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './page/homepage/homepage.component';
 import './App.css';
+
+import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './page/sing-in-and-sign-up/sing-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
+const App = () => {
+
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {    
+    const unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      setUser(user);      
+    });
+
+    return (() => {
+      unsubscribeFromAuth();
+    });
+  }, []);
+
   return (
     <div>
-      <Header />
+      <Header user={user} />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({user}) => (
   <div className='header'>
     <Link className='logo-container' to="/"><Logo className='logo' /></Link>
     <div className='options'>
       <Link className='option' to="/shop">SHOP</Link>
       <Link className='option' to="/contact">CONTACT</Link>
-      <Link className='option' to="/signin">SIGN IN</Link>
+      {
+        user ?
+        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        : <Link className='option' to="/signin">SIGN IN</Link>
+      }
+      
     </div>
   </div>
 )

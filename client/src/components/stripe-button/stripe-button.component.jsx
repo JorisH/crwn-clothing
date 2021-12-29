948a5ce85ce5d1ca0f3f7b60953ec60fa5f4a0cd
Joris Hontelé <joris@tactics.be>
2021-12-29 18:10:15 +0100

    move client code to client and added server to handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import StripeCheckout from "react-stripe-checkout"
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const stripeAmount = price * 100;
   const stripeKey = "pk_test_51KABQSCG0E5ZDPs2tAeLKFaMzalOoroBmmnjPp4Cp4iToyxw3GHewHToOkUJOCKu049J1YFkA0ffQWc8RxhYvewi00qW91be60"
   const onToken = (token) => {
-    alert("Payment Succesful");
-    console.log(token)
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: stripeAmount,
+        token
+      }
+    }).then(response => {
+      alert(("Payment successful"));
+    }).catch(error => {
+      console.log("Payment error: ", JSON.parse(error));
+      alert(
+        "There was an issue with your payment. Please make sure you use the provided credit card."
+      );
+    });
   }
 
   return (
